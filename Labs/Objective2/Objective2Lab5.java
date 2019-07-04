b296859b00227a326064879639d9e94cfca85099
jdebroux <jtdebroux@gmail.com>
2019-07-03 22:54:58 -0600

    completed Objective2Lab5

---
+++ Labs/Objective2/Objective2Lab5.java
@@ -0,0 +1,13 @@
+public class Objective2Lab5 {
+  public static void main(String[] args) {
+
+    double side1, side2, hypotenuse;
+
+    side1 = 6.2;
+    side2 = 11.3;
+    hypotenuse = Math.sqrt((side1*side1) + (side2*side2));
+
+    System.out.println("The hypotenuse of a triangle with sides 6.2 and 11.3 is " + hypotenuse);
+
+  }
+}
