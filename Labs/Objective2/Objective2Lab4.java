098963a3f3a323f39418ed6bd0c3d848a7b7e44b
jdebroux <jtdebroux@gmail.com>
2019-07-03 22:43:06 -0600

    completed Objective2Lab4

---
+++ Labs/Objective2/Objective2Lab4.java
@@ -0,0 +1,14 @@
+public class Objective2Lab4 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average, x;
+
+    num1 = 6.7;
+    num2 = 7.6;
+    num3 = 67.76;
+    num3 = num1+num2+num3;
+    average = num3/3;
+
+    System.out.println("The average of 6.7, 7.6, and 67.76 is " + average);
+
+  }
+}
