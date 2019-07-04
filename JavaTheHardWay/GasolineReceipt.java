04cf43cc0ce6b12bbe08c12efdcf0ec1e2905a82
jdebroux <jtdebroux@gmail.com>
2019-07-03 20:02:23 -0600

    finish jthw ex 2

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt{
+  public static void main( String[] args ) {
+      System.out.println( "+-----------------------------------+" );
+      System.out.println( "|                                   |" );
+      System.out.println( "|           CORNER STORE            |" );
+      System.out.println( "|                                   |" );
+      System.out.println( "|       2019-07-03   06:55pm        |" );
+      System.out.println( "|                                   |" );
+      System.out.println( "|     Gallons:          10.870      |" );
+      System.out.println( "|     Price/gallon:    $ 3.989      |" );
+      System.out.println( "|                                   |" );
+      System.out.println( "|     Fuel total:     $ 43.36       |" );
+      System.out.println( "|                                   |" );
+      System.out.println( "+-----------------------------------+" );
+  }
+}
