8d2782fd59143d9065e5cb9c9cc3ede7818afd87
jdebroux <jtdebroux@gmail.com>
2019-07-04 15:51:13 -0600

    Completed Objective3Lab5

---
+++ Labs/Objective3/Objective3Lab5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Objective3Lab5 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    Double num1, num2, num3;
+
+    System.out.println("Please enter a number: ");
+    num1 = input.nextDouble();
+
+    System.out.println("Please enter another number: ");
+    num2 = input.nextDouble();
+
+    num3 = num1+num2;
+
+    System.out.println("The sum of " + num1 + " + " + num2 + " = " + num3);
+
+    input.close();
+
+  }
+}
