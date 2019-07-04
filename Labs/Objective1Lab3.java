05f3180ef01715d6b025dee1e2ba8e13c23ae7eb
jdebroux <jtdebroux@gmail.com>
2019-07-03 20:29:50 -0600

    complete Objective1Lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3{
+  public static void main(String[] args ){
+    System.out.print("Name : Joseph DeBroux\n");
+    System.out.println("Age" + " " + ":" + " " + "29");
+    System.out.println("State : Wisconsin");
+  }
+}
