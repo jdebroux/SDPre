92174f6972a234f05d6860daf48b5c0c94ecd40d
jdebroux <jtdebroux@gmail.com>
2019-07-04 23:56:50 -0600

    Completed jthw Objective4Lab1

---
+++ Labs/Objective4/Objective4Lab1.java
@@ -0,0 +1,17 @@
+public class Objective4Lab1 {
+  public static void main(String[] args) {
+    int num1 = 5;
+    int num2 = 15;
+
+    /* Objective says to "Fill in the following logic conditions" here, but
+    not entirely sure what this message is asking.  I did write the code to 
+    compile and print the expected output*/
+
+    if(num1>num2) {
+      System.out.println(num1 + " is GREATER THAN " + num2);
+    }
+    else if (num2 > num1) {
+      System.out.println(num2 + " is GREATER THAN " + num1);
+    }
+  }
+}
